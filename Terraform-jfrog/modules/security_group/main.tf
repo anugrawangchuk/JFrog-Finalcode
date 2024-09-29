ed9f110997a6757083de9e994eae6e0333a6b473
Anugra Wangchuk Lepcha <106028390+anugrawangchuk@users.noreply.github.com>
2024-09-30 02:30:09 +0530

    Update main.tf

--- Terraform-jfrog/modules/security_group/main.tf
+++ Terraform-jfrog/modules/security_group/main.tf
@@ -1,23 +1,36 @@
 # Security Group for Public Instance
 resource "aws_security_group" "public_sg" {
-  name        = "public-instance-sg"
-  description = "Allow all inbound traffic to the public instance"
-  vpc_id      = var.vpc_id
+    name = "public-instance-sg"
+    description = "Allow inbound traffic to the public instance"
+    vpc_id = var.vpc_id
 
-  # Allow all inbound traffic
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"  # All protocols
-    cidr_blocks = ["0.0.0.0/0"]  # Allow from anywhere
+
+    ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # SSH from anywhere
   }
 
-  # Allow all outbound traffic
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # HTTP access from anywhere
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # HTTPS access from anywhere
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"  # All protocols
-    cidr_blocks = ["0.0.0.0/0"]  # Allow to anywhere
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow all outbound traffic
   }
 
   tags = {
@@ -31,20 +44,18 @@ resource "aws_security_group" "private_sg" {
   description = "Allow internal VPC communication for private instance"
   vpc_id      = var.vpc_id
 
-  # Allow all inbound traffic (for testing purposes)
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"  # All protocols
-    cidr_blocks = ["0.0.0.0/0"]  # Allow from anywhere (consider restricting this)
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]  # Allow SSH from within the VPC
   }
 
-  # Allow all outbound traffic
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"  # All protocols
-    cidr_blocks = ["0.0.0.0/0"]  # Allow to anywhere
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow all outbound traffic
   }
 
   tags = {
