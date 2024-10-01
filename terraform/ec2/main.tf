78f6a69221dfcbe5252e99e207ab4916013ccfd9
Anugra Wangchuk Lepcha <106028390+anugrawangchuk@users.noreply.github.com>
2024-10-01 21:13:51 +0530

    Update main.tf

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -7,6 +7,10 @@ resource "aws_instance" "bastion" {
   subnet_id     = var.public_subnet_id
   security_groups = [var.bastion_sg_id]
 
+root_block_device {
+    volume_size = 10
+  }
+
   tags = {
     Name = "Bastion Host"
   }
@@ -19,6 +23,10 @@ resource "aws_instance" "private_ec2" {
   key_name      = var.key_name
   subnet_id     = var.private_subnet_id
   security_groups = [var.private_sg_id] # Use security_group_ids here
+
+root_block_device {
+    volume_size = 40
+  }
 
   tags = {
     Name = "Private EC2"
